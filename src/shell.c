7e656cf424829903171dfe54e1a24cee8740485a
diegopovalz <alejopov@gmail.com>
2023-09-25 15:42:16 -0500

    add ls too

--- src/shell.c
+++ src/shell.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int loop(){
@@ -17,6 +19,11 @@ int loop(){
             continue;
         }
 
+        /* if (strncmp(cmd_str, "cd", 2) == 0) {
+            fprintf(stderr, "wish: cd: missing argument\n");
+            continue;
+        } */
+
         if (strncmp(cmd_str, "cd ", 3) == 0) {
             token = strtok(cmd_str + 3, " \t\n");
             if (token == NULL) {
@@ -28,6 +35,29 @@ int loop(){
             }
             continue;
         }
+
+        if (strncmp(cmd_str, "ls", 2) == 0) {
+            pid_t pid, wpid;
+            int status;
+
+            pid = fork();
+            if (pid == 0) {
+                char *args[] = {"ls", NULL};
+                execvp(args[0], args);
+                
+                perror("wish");
+                exit(EXIT_FAILURE);
+            } else if (pid < 0) {
+                perror("wish");
+            } else {
+                do {
+                    wpid = waitpid(pid, &status, WUNTRACED);
+                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+            }
+
+            continue;
+        }
+
         printf("%s\n", cmd_str);
     }
     free(cmd_str);
