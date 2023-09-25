92c0e81d49c17360b9dee24aa13761b12b4f16f1
diegopovalz <alejopov@gmail.com>
2023-09-25 15:26:29 -0500

    start implementation of cd

--- src/shell.c
+++ src/shell.c
@@ -2,14 +2,34 @@
 #include "../include/prompt.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int loop(){
     int running = 1;
     char* cmd_str;
+    char* token;
+
     while (running) {
         cmd_str = readline();
+        if (!strcmp(cmd_str, "exit")) {
+            running = 0;
+            continue;
+        }
+
+        if (strncmp(cmd_str, "cd ", 3) == 0) {
+            token = strtok(cmd_str + 3, " \t\n");
+            if (token == NULL) {
+                fprintf(stderr, "wish: cd: missing argument\n");
+            } else {
+                if (chdir(token) != 0) {
+                    perror("wish");
+                }
+            }
+            continue;
+        }
         printf("%s\n", cmd_str);
-        if (!strcmp(cmd_str, "exit")) running = 0;
     }
+    free(cmd_str);
     return running;
 }
