83f275aafbc5be9753f92f1bf30fc11b3ad59925
diegopovalz <alejopov@gmail.com>
2023-09-28 19:34:07 -0500

    add possibility to cd to home directory

--- src/builtin.c
+++ src/builtin.c
@@ -1,4 +1,6 @@
+#include "../include/global.h"
 #include "../include/builtin.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,6 +9,15 @@ void exec_exit(){
     exit(0);
 }
 
-void change_directory(char *dir){
-    if (chdir(dir) != 0) perror("wish");
+void change_directory(char *dir) {
+    if (dir[0] == '~') { 
+        char path[MAX_DIR_SIZE] = "";   // Ojalá no se haga cd con un directorio que se lleve más de 1024 caracteres
+        char *home = getenv("HOME");
+        strcat(path, home);
+        strcat(path, dir + 1);
+        dir = path;
+    }   
+    if (chdir(dir) != 0) {
+        perror("wish");
+    }
 }
